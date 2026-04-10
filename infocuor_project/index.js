c8e2df47ea91e94c998ff0f926dbb1cd4f2fcaa7
ishara <isharamorawaka2002@gmail.com>
2026-04-10 07:50:41 +0530

    mongoose👍

--- infocuor_project/index.js
+++ infocuor_project/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 import userRouter from './route/userRouter.js';
 import eventRouter from './route/eventRouter.js';
 import bookingRouter from './route/bookingRouter.js';
+import mongoose from 'mongoose';
 
 const app=express();
+
+mongoose.connect("mongodb+srv://chalanaishara563_db_user:123@web.8unhd4n.mongodb.net/infocuor?appName=web").then(()=>{
+    console.log("connected to the database")
+}).catch(()=>{
+    console.log("connection field")
+})
 
 app.use("users",userRouter)
 app.use("events",eventRouter)
